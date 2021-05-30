89ae7d581606a76e53f34b2904307a0e7c53224c
ahmadfma <ahmadfathanah05@gmail.com>
2021-05-30 19:58:01 +0800

    Create GoalsFragment

---
+++ app/src/main/java/com/example/moneymanagement/UI/GoalsFragment/GoalsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.moneymanagement.UI.GoalsFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moneymanagement.R
+
+class GoalsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goals, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = GoalsFragment()
+    }
+}
