5f63c6260f7133fd828349cb1e03cb9870138312
ahmadfma <ahmadfathanah05@gmail.com>
2021-05-31 22:37:15 +0800

    Create TransactionEntity Class

---
+++ app/src/main/java/com/example/moneymanagement/User/TransactionData/TransactionEntity.kt
@@ -0,0 +1,14 @@
+package com.example.moneymanagement.User.TransactionData
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_transactions")
+data class TransactionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var type: String,
+    var amount: Int,
+    var title: String,
+    var date: String
+)
