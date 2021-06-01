17a91f7686dd8a1b18cc6de9228e6d7a0ec4a265
ahmadfma <ahmadfathanah05@gmail.com>
2021-06-01 22:25:24 +0800

    Create item_transaction_dialog layout and display it when transaction clicked

--- app/src/main/java/com/example/moneymanagement/UI/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/moneymanagement/UI/HomeFragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +14,7 @@ import com.example.moneymanagement.R
 import com.example.moneymanagement.User.TransactionData.TransactionEntity
 import com.example.moneymanagement.User.UserViewModel
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_transaction_dialog.view.*
 
 class HomeFragment : Fragment() {
 
@@ -38,7 +40,41 @@ class HomeFragment : Fragment() {
     }
 
     private fun onViewAction(transaction: TransactionEntity) {
-        Toast.makeText(context, "${transaction.title}", Toast.LENGTH_SHORT).show()
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.item_transaction_dialog, null)
+        val builder = context?.let {
+            AlertDialog.Builder(it)
+                .setView(dialogView)
+        }
+        val mDialog = builder?.show()
+        dialogView.close_btn.setOnClickListener {
+            mDialog?.dismiss()
+        }
+        dialogView.judulET.setText(transaction.title)
+        dialogView.jumlahET.setText(transaction.amount.toString())
+        dialogView.tanggalET.isEnabled = false
+        dialogView.tanggalET.setText(transaction.date)
+        dialogView.tipeET.isEnabled = false
+        dialogView.tipeET.setText(transaction.type)
+        when(transaction.category) {
+            "Makanan & Minuman" -> {
+                dialogView.makanan_minuman.isChecked = true
+            }
+            "Kecantikan & Kesehatan" -> {
+                dialogView.kecantikan_kesehatan.isChecked = true
+            }
+            "Sosial & Gaya Hidup" -> {
+                dialogView.sosial_gayahidup.isChecked = true
+            }
+            "Entertainment" -> {
+                dialogView.entertainment.isChecked = true
+            }
+            "Transportasi" -> {
+                dialogView.transportasi.isChecked = true
+            }
+            "Lainnya" -> {
+                dialogView.lainnya.isChecked = true
+            }
+        }
     }
 
     companion object {
