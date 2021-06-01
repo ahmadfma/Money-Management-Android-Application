440feba5ae88e2607890aecc09860e6790d13230
ahmadfma <ahmadfathanah05@gmail.com>
2021-06-01 23:15:26 +0800

    Create SaldoEntity Class

---
+++ app/src/main/java/com/example/moneymanagement/User/Saldo/SaldoEntity.kt
@@ -0,0 +1,11 @@
+package com.example.moneymanagement.User.Saldo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_saldo")
+data class SaldoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var saldo: Int
+)
