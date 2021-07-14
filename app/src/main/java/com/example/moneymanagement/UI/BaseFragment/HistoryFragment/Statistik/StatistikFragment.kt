7dbab3b764f27fc36fe660511023853d2f55e8ff
ahmadfma <ahmadfathanah05@gmail.com>
2021-07-14 21:15:25 +0800

    Show Statistic with Pie Chart

--- app/src/main/java/com/example/moneymanagement/UI/BaseFragment/HistoryFragment/Statistik/StatistikFragment.kt
+++ app/src/main/java/com/example/moneymanagement/UI/BaseFragment/HistoryFragment/Statistik/StatistikFragment.kt
@@ -1,10 +1,12 @@
 package com.example.moneymanagement.UI.BaseFragment.HistoryFragment.Statistik
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -13,6 +15,11 @@ import com.example.moneymanagement.R
 import com.example.moneymanagement.UI.BaseFragment.HistoryFragment.HistoryViewModel
 import com.example.moneymanagement.User.UserViewModel
 import com.example.moneymanagement.Utilities.Utilities
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import kotlinx.android.synthetic.main.item_history_fragment_statistik.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,6 +30,12 @@ class StatistikFragment : Fragment() {
 
     private lateinit var userViewModel: UserViewModel
     private lateinit var historyViewModel: HistoryViewModel
+    private var totalPemasukan: Long? = 0
+    private var totalPengeluaran: Long? = 0
+    private var listPengeluaranByCategory: ArrayList<Long?> = arrayListOf()
+    private var listPemasukanByCategory: ArrayList<Long?> = arrayListOf()
+    private val listPercentPemasukan = mutableListOf<Float>()
+    private val listPercentPengeluaran = mutableListOf<Float>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -66,33 +79,90 @@ class StatistikFragment : Fragment() {
                 loadStatisticBasedOnMonth(month)
             }
         })
+        loadStatisticBasedOnMonth(set[0])
     }
 
     private fun loadStatisticBasedOnMonth(month: String) = lifecycleScope.launch(Dispatchers.IO) {
+        clearList()
         loadTotalPemasukan(month)
         loadTotalPengeluaran(month)
+        loadStatistic(pieChartPemasukan, "Pemasukan", totalPemasukan, listPemasukanByCategory, listPercentPemasukan)
+        loadStatistic(pieChartPengeluaran, "Pengeluaran", totalPengeluaran, listPengeluaranByCategory, listPercentPengeluaran)
+    }
+
+    private fun clearList() {
+        listPemasukanByCategory.clear()
+        listPengeluaranByCategory.clear()
+        listPercentPemasukan.clear()
+        listPercentPengeluaran.clear()
     }
 
     private suspend fun loadTotalPemasukan(month: String) {
         withContext(lifecycleScope.coroutineContext + Dispatchers.IO) {
-            val pemasukan = userViewModel.getTotalAmountByMonth("%$month%", "pemasukan")
-            Log.d(TAG, "total pemasukan bulan $month = $pemasukan")
+            totalPemasukan = userViewModel.getTotalAmountByMonth("%$month%", "pemasukan")
+            Log.d(TAG, "total pemasukan bulan $month = $totalPemasukan")
             Utilities.listKateogri().forEach {
                 val totalamount = userViewModel.getTotalAmountByMonthAndCategory("%$month%", "pemasukan", it)
                 Log.d(TAG, "total pemasukan bulan $month = $totalamount, kategori $it")
+                listPemasukanByCategory.add(totalamount)
             }
         }
     }
 
     private suspend fun loadTotalPengeluaran(month: String) {
         withContext(lifecycleScope.coroutineContext + Dispatchers.IO) {
-            val pengeluaran = userViewModel.getTotalAmountByMonth("%$month%", "pengeluaran")
-            Log.d(TAG, "total pengeluran bulan $month = $pengeluaran")
+            totalPengeluaran = userViewModel.getTotalAmountByMonth("%$month%", "pengeluaran")
+            Log.d(TAG, "total pengeluran bulan $month = $totalPengeluaran")
             Utilities.listKateogri().forEach {
                 val totalamount = userViewModel.getTotalAmountByMonthAndCategory("%$month%", "pengeluaran", it)
                 Log.d(TAG, "total pengeluaran bulan $month = $totalamount, kategori $it")
+                listPengeluaranByCategory.add(totalamount)
             }
         }
+    }
+
+    private fun loadStatistic(pieChart: PieChart, type: String, total: Long?, listAmount: ArrayList<Long?>, listPercent: MutableList<Float>) {
+        listPercentPemasukan.clear()
+        listPercentPengeluaran.clear()
+
+        listAmount.forEach {
+            if(it != null && it != 0L) {
+                val percent = it.toFloat() / total!!.toFloat() * 100
+                listPercent.add(percent)
+            } else {
+                listPercent.add(0F)
+            }
+        }
+
+        //load pie chart
+
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setUsePercentValues(true)
+        pieChart.setDrawEntryLabels(false)
+        pieChart.centerText = type
+        pieChart.description.isEnabled = false
+        pieChart.legend.isEnabled = false
+
+        val colors = arrayListOf<Int>()
+        val entries = arrayListOf<PieEntry>()
+        Utilities.listKateogri().forEachIndexed { index, s ->
+            if(listPercent[index] != 0.0F) {
+                entries.add(PieEntry(listPercent[index], s))
+                colors.add(ContextCompat.getColor(requireContext(), Utilities.listKateogriColor()[index]))
+            }
+        }
+
+        val dataset = PieDataSet(entries, type)
+        dataset.colors = colors
+
+        val data = PieData(dataset)
+        data.setDrawValues(true)
+        data.setValueFormatter(PercentFormatter(pieChartPemasukan))
+        data.setValueTextSize(10f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.data = data
+        pieChart.invalidate()
     }
 
     companion object {
